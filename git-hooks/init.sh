d51b40329d602e4b63f835a81872997b2b4f1d8d
Danny Grasse <Danny.Grasse@sas.com>
2020-06-05 14:01:22 -0400

    some (maybe final?) refinements to git-hook logic

--- git-hooks/init.sh
+++ git-hooks/init.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# RUN THIS FILE AS AN EXECUTABLE FROM IT'S OWN DIRECTORY
+# IT IS BENIGN TO RUN IT WHENEVER YOU WANT, BUT IS ONLY NEEDED THE FIRST TIME YOU DO A CLONE
+
 git config core.hooksPath git-hooks
+
+cd ..
+npm install 
+
+
+rm -fr target/dependency/*
+mvn install dependency:copy-dependencies
 
